a8383e14fe3db41820b1a36c9e0435ace51ef502
phystech-aa <volkova.aa@phystec.edu>
2021-02-04 17:54:37 +0300

    start

---
+++ cmake-build-debug/CMakeFiles/cpp_4sem.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_4sem.dir/main.cpp.obj"
+  "cpp_4sem.exe"
+  "cpp_4sem.exe.manifest"
+  "cpp_4sem.lib"
+  "cpp_4sem.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_4sem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/cpp_4sem.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cpp_4sem.
+# This may be replaced when dependencies are built.
