2b2c6d72d17d8c42c2cb614dd51c63b7fc8b30f9
phystech-aa <volkova.aa@phystec.edu>
2021-02-06 21:36:38 +0300

    first part of changes in sourse file

--- sourse.cpp
+++ sourse.cpp
@@ -1,6 +1,19 @@
 //
  
 #include<iostream>
+#include<chrono> //интервалы и моменты
+#include<vector>
+#include<algorithm>
+
+class Timer{
+public:
+    using clock_t = std::chrono::steady_clock;
+    using timepoint_t = clock_t ::time_point ;
+    Timer() : m_begin(clock_t::now()) {}
+    ~Timer(){
+        auto end = clock_t::now();
+        std::cout <<  "milliseconds: " <<
+        std::chrono::duration_cast <std::chrono::milliseconds > (end - m_begin).count() ;
 
 void f(){}
 
