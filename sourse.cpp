d9d7e5e353a86ae1b3f16d0a88255148ac8b6ef1
phystech-aa <volkova.aa@phystec.edu>
2021-02-06 21:36:50 +0300

    second part of changes in sourse file

--- sourse.cpp
+++ sourse.cpp
@@ -15,12 +15,45 @@ public:
         std::cout <<  "milliseconds: " <<
         std::chrono::duration_cast <std::chrono::milliseconds > (end - m_begin).count() ;
 
-void f(){}
+    }
+
+private:
+    timepoint_t  m_begin;
+};
 
 int main(){
 
-    std::cout<<"j";
-    //hello from master
+    // std::ratio <1, 3> r;
+
+    // интервал -- n тактов заданной продолжительности
+    // timepoint - комбинация интервала и эпохи
+    // эпоха - начало отсчёта
+    // clock - опр эпоху и мин такт
+
+    //std::chrono::duration<int, std::ratio<1, 1>> a(42);
+    //std::chrono::duration<double, std::ratio<60>> b(0.5);
+
+    std::chrono::seconds a(20);
+    std::chrono::hours b(2);
+    std::chrono::microseconds c(42);
+
+    c += a+b;
+    --c;
+    c *= 2;
+
+    std::cout << c.count() << "\n";
+
+    // system_clock - OS
+    // staedy_clock - не допускают подведения
+    // high_resolution_clock - с мин доступным тактом
+
+    std::vector v(100000, 0);
+    for (auto i = 0U; i<std::size(v); ++i) {
+        v[i] = 1000U - i;
+    }
+
+    Timer t;
+    std::sort(std::begin(v), std::end(v));
 
     return 0;
 }
