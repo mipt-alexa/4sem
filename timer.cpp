a6d30dfb5b9cd4af622aa18452b5e733b58fe502
phystech-aa <volkova.aa@phystec.edu>
2021-02-07 19:07:04 +0300

    timer file changed (apparently i misunderstood the task previous time)

--- timer.cpp
+++ timer.cpp
@@ -11,41 +11,42 @@ public:
     Timer() {
         m_begin = clock_t::now();
         last_timestamp = clock_t::now();
+        time = std::chrono::milliseconds (0);
     }
     ~Timer(){
         auto end = clock_t::now();
         std::cout <<  "total time is " <<
-                  std::chrono::duration_cast <std::chrono::milliseconds > (end - m_begin).count()
-                  <<  " milliseconds\n" ;
+                  std::chrono::duration_cast <std::chrono::seconds > (end - m_begin).count()
+                  <<  "seconds\n" ;
     }
 
-    void stop(){             // prints the duration using the current epoch (since the last restart)
+    void stop(){
         auto current = clock_t::now();
-        std::cout <<
-                  std::chrono::duration_cast <std::chrono::seconds > (current - last_timestamp).count()
-                  <<   " second(s) passed\n";
+        time += std::chrono::duration_cast <std::chrono::milliseconds>(current - last_timestamp);
+        std::cout << "stop, " << time.count() <<   " millisecond(s) passed\n";
     }
 
-    void restart(){         // sets up a new epoch
+    void run(){
         last_timestamp = clock_t::now();
-        std::cout << "timer is restarted\n";
+        std::cout << "timer is on\n";
     }
 
 private:
     timepoint_t  m_begin;
     timepoint_t last_timestamp;
+    std::chrono::milliseconds time;
 };
 
 int main(){
 
     Timer t;
     int a = 1;
-    while (a != 0) {
+    while (a != 0) {        // 1 for stop, 2 for start
         std::cin >> a;
         if (a == 1)
             t.stop();
         if (a == 2)
-            t.restart();
+            t.run();
     }
 
     return 0;
