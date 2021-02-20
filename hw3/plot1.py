a8c0488d6eef659e0c4aed30fc24af48db8bd79f
phystech-aa <volkova.aa@phystec.edu>
2021-02-20 11:55:11 +0300

    task 3-1 changed

--- hw3/plot1.py
+++ hw3/plot1.py
@@ -3,20 +3,10 @@ import numpy
 
 f = open("mydata.txt")
 
-X = []
-Y1 = []
-Y2 = []
-
-for line in f.readlines():
-    lst = line.split()
-    X.append(int(lst[0]))
-    Y1.append(int(lst[1]))
-    Y2.append(int(lst[2]))
+X, Y1, Y2 = zip(*[map(int, line.strip().split()) for line in f.readlines()])
 
 for i in X:
-    i = i*numpy.log(i)
-
-print(X)
+    i = i * numpy.log(i)
 
 plt.scatter(X, Y1, label="set.insert()")
 plt.scatter(X, Y2, label="sort(vector)")
@@ -24,4 +14,5 @@ plt.scatter(X, Y2, label="sort(vector)")
 plt.xlabel("N log(N)")
 plt.ylabel("milliseconds")
 plt.legend()
-plt.show()
+# plt.show()
+plt.savefig('Figure_1.png')
