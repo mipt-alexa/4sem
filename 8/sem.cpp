97a8659f02ed199d18ccd2d7faf3e6eb55844f4a
phystech-aa <volkova.aa@phystec.edu>
2021-03-27 12:38:18 +0300

    seminar 8 was held

---
+++ 8/sem.cpp
@@ -0,0 +1,18 @@
+#include <mutex>
+#include <thread>
+#include <atomic>
+
+
+void f()}{}
+
+int main(){
+
+    std::once_flag flag;
+
+    std::call_once(flag, f);
+
+    std::atomic <int> a;
+    std::cout << std::boolal
+
+
+}
